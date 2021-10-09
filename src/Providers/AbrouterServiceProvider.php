4538ed7b8d7b5d064c489934a9c9f2e1674245a2
Yatsenko <webfay1@gmail.com>
2021-10-09 14:04:02 +0300

    Fix namespace for provider

--- src/Providers/AbrouterServiceProvider.php
+++ src/Providers/AbrouterServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace Abrouter\LaravelClient;
+namespace Abrouter\LaravelClient\Providers;
 
 use Abrouter\Client\Config\Config;
 use Illuminate\Support\ServiceProvider;
